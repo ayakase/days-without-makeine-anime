fc9b109285c2f1a3229b1fa9a00909f9a25c1605
ayakase <wibu1892001@gmail.com>
2024-10-09 00:06:48 +0700

    [add] i18n, hero video

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,7 @@ import CountdownTimer from "../../components/Particles/Counter";
 import { CoolMode } from "../../components/ui/cool-mode";
 import { AnimatedSubscribeButton } from "../../components/ui/animated-subscribe-button";
 import GradualSpacing from "../../components/ui/gradual-spacing";
+import HeroVideoDialog from "../../components/ui/hero-video-dialog";
 import "./index.css";
 // import Comp from "../../components/Animations/PhysicTank";
 export function Home() {
@@ -14,15 +15,25 @@ export function Home() {
       <div className="background">
         <div className="h-screen w-screen grid place-items-center">
           <div>
+            <div className="relative w-96">
+              <HeroVideoDialog
+                className="dark:hidden block"
+                animationStyle="top-in-bottom-out"
+                videoSrc="https://www.youtube.com/embed/uytJ6_KTCZI?si=u-IYoXIfMwOm4QHB"
+                thumbnailSrc="https://animetv-jp.net/wp-content/uploads/2024/08/GUEoK1naUAA0I2--e1722712211505-696x392.jpg"
+                thumbnailAlt="Hero Video"
+              />
+            </div>
             {/* <LetterPullup
               words="Days without Too Many Losing Heroines! anime"
               className="text-yellow-400 !text-6xl mb-10"
               delay={0.05}
             ></LetterPullup> */}
-            <GradualSpacing
+
+            {/* <GradualSpacing
               className="font-display text-center text-4xl font-bold -tracking-widest  text-yellow-400  md:text-7xl my-5"
               text="Days without Too Many Losing Heroines! anime"
-            ></GradualSpacing>
+            ></GradualSpacing> */}
             <CountdownTimer></CountdownTimer>
             <div className="w-full flex justify-center mt-2">
               <CoolMode
